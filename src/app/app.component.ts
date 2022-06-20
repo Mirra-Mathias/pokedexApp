1d8755653941f89301d6cc448b6cd9d2539102b3
Mirra Mathias <52259162+Mirra-Mathias@users.noreply.github.com>
2022-06-20 19:49:06 +0200

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokedex-app';
+  private name: any;
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { CompteComponent } from './compte/compte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PokemonsComponent,
+    CompteComponent
   ],
   imports: [
     BrowserModule,
