9b3e8dca0c9b3a49470f60f7f83e278a4c5401eb
CLarroque <cecile.larroque6@gmail.com>
2022-06-20 21:35:41 +0200

    Page details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {PokemonsComponent} from "./pokemons/pokemons.component";
 import {CompteComponent} from "./compte/compte.component";
 import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
 const routes: Routes = [
   {path: 'footer', component: FooterComponent},
@@ -14,7 +15,7 @@ const routes: Routes = [
   {path: 'compte', component: CompteComponent},
   {path: 'login', component: LoginComponent},
   {path: '', component: HomeComponent},
-  {path: 'pokemonsDetails/:id', component: CompteComponent}];
+  {path: 'pokemons/:name-:id', component: PokemonDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {RoutesService} from "../routes.service";
+import {PokemonsService} from "../pokemons.service";
+import {ActivatedRoute} from '@angular/router';
 
+export interface Pokemon {
+  baseAttack?: number,
+  baseDefense?: number,
+  baseHP?: number,
+  baseSp_Attack?: number,
+  baseSp_Defense?: number,
+  baseSpeed?: number,
+  id?: number,
+  name?: string,
+  type0?: string,
+  type1?: string,
+}
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
   styleUrls: ['./pokemon-details.component.css']
 })
 export class PokemonDetailsComponent implements OnInit {
+  public pokemons: Pokemon[] = [];
+  public pokemon: Pokemon = {};
 
-  constructor() { }
+  constructor(private route:RoutesService, public pokemonService:PokemonsService, private routeA: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeA.queryParams.subscribe(async (params) => {
+      this.route.getPokemons("french").then((pokemons:Pokemon[]) => {
+        this.pokemons = pokemons
+      })
+    })
+    
+
+
   }
 
 }
