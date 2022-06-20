20ecc40622ae04b2f9289f6a3437d4b09afa8624
Adrien3111 <abourrel@mcgr.fr>
2022-06-20 20:09:30 +0200

    ajout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
+import {PokemonsComponent} from "./pokemons/pokemons.component";
+import {CompteComponent} from "./compte/compte.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {path: 'footer', component: FooterComponent},
-  {path: 'header', component: HeaderComponent}];
+  {path: 'header', component: HeaderComponent},
+  {path: 'pokemons', component: PokemonsComponent},
+  {path: 'compte', component: CompteComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'pokemonsDetails/:id', component: CompteComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
